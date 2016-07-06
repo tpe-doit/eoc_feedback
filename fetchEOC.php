9666ab5880e48181620f3aca21233c6db3036df5
yungshin <yungshin.chien@gmail.com>
2016-07-06 17:36:21 +0800

    add fetchEOC script

---
+++ fetchEOC.php
@@ -0,0 +1,84 @@
+<?php
+
+$eoc_data_url = 'https://tcgbusfs.blob.core.windows.net/blobfs/GetDisasterSummary.gz';
+$data_gzip = file_get_contents($eoc_data_url);
+$raw_data = gzdecode($data_gzip);
+$raw_data_array = json_decode($raw_data, true);
+$eoc_data_list = $raw_data_array['DataSet']['diffgr:diffgram']['NewDataSet']['CASE_SUMMARY'];
+$inserted_count = 0;
+$update_count = 0;
+
+// connect db
+$db_host = getenv('DB_HOST');
+$dsn = "mysql:dbname=eoc_feedback;host={$db_host}";
+$db_user = getenv('DB_USERNAME');
+$db_pass = getenv('DB_PASSWORD');
+try {
+    $dbh = new PDO($dsn, $db_user, $db_pass);
+} catch (PDOException $e) {
+    echo 'Connection failed: ' . $e->getMessage();
+}
+
+foreach ($eoc_data_list as $eoc_data) {
+    // check duplicate
+    $sql = 'select * from cases where DPName=? and CaseSN=?';
+    $sth = $dbh->prepare($sql);
+    $sth->execute(array($eoc_data['DPName'], intval($eoc_data['CaseSN'])));
+    $case_count = $sth->rowCount();
+
+    if ($case_count !== 0) {
+        // remove useless data
+        unset($eoc_data['-diffgr:id']);
+        unset($eoc_data['-msdata:rowOrder']);
+        // transform some data to correct type
+        $eoc_data['CaseID'] = trim($eoc_data['CaseID']);
+        $eoc_data['CaseSN'] = intval($eoc_data['CaseSN']);
+        if ($eoc_data['CaseComplete'] === 'false') {
+            $eoc_data['CaseComplete'] = false;
+        } else {
+            $eoc_data['CaseComplete'] = true;
+        }
+        $eoc_data['Wgs84X'] = doubleval($eoc_data['Wgs84X']);
+        $eoc_data['Wgs84Y'] = doubleval($eoc_data['Wgs84Y']);
+        // update data to database
+        $sql = "update cases set DPName=:DPName, CaseID=:CaseID, CaseSN=:CaseSN, CaseTime=:CaseTime,
+            Name=:Name, CaseLocationDistrict=:CaseLocationDistrict, CaseLocationDescription=:CaseLocationDescription,
+            CaseDescription=:CaseDescription, CaseComplete=:CaseComplete, PName=:PName, Wgs84X=:Wgs84X,
+            Wgs84Y=:Wgs84Y where DPName=:DPname and CaseSN=:CaseSN";
+        $sth = $dbh->prepare($sql);
+        $result = $sth->execute($eoc_data);
+        if ($result) {
+            $update_count++;
+        }
+    } else {
+        // remove useless data
+        unset($eoc_data['-diffgr:id']);
+        unset($eoc_data['-msdata:rowOrder']);
+        // transform some data to correct type
+        $eoc_data['CaseID'] = trim($eoc_data['CaseID']);
+        $eoc_data['CaseSN'] = intval($eoc_data['CaseSN']);
+        if ($eoc_data['CaseComplete'] === 'false') {
+            $eoc_data['CaseComplete'] = false;
+        } else {
+            $eoc_data['CaseComplete'] = true;
+        }
+        $eoc_data['Wgs84X'] = doubleval($eoc_data['Wgs84X']);
+        $eoc_data['Wgs84Y'] = doubleval($eoc_data['Wgs84Y']);
+        // insert data to database
+        $sql = "insert into cases (DPName, CaseID, CaseSN, CaseTime, Name, CaseLocationDistrict,
+            CaseLocationDescription, CaseDescription, CaseComplete, PName, Wgs84X, Wgs84Y) values (
+            :DPName, :CaseID, :CaseSN, :CaseTime, :Name, :CaseLocationDistrict,
+            :CaseLocationDescription, :CaseDescription, :CaseComplete, :PName, :Wgs84X, :Wgs84Y)";
+        $sth = $dbh->prepare($sql);
+        $result = $sth->execute($eoc_data);
+        if ($result) {
+            $inserted_count++;
+        }
+    }
+}
+
+echo json_encode([
+    'message' => 'Fetch succeed.',
+    'inserted_count' => $inserted_count,
+    'update_count' => $update_count
+]);
